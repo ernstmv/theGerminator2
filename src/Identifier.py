b12e787f6a462f2b50d16455a7380132af65bcb3
ernstmv <ernestoroque777@gmail.com>
2024-04-23 18:25:42 -0600

    Plant identifier implemented

--- src/Identifier.py
+++ src/Identifier.py
@@ -1,23 +1,4 @@
 import cv2
-import numpy as np
-
-'''
-This class finds the coordinates from each plant detected acording with the
-input values (thresh_v, min_a).
-
-Attributes:
-    self.img
-    self.green_layer
-    self.green_threshed
-
-Methods:
-    __init__                            Void constructor
-    set_image(image)                    Set self.img
-    extract_green()                     Set self.green_layer
-    process_image(thresh_v)             Set self.green_threshed
-    extract_coordinates(min_a)          Find the x, y coordinates of each plant
-    find_plants(img, thresh_v, min_a)   Consumes the above functions
-'''
 
 
 class Identifier:
@@ -25,47 +6,38 @@ class Identifier:
     def __init__(self):
         pass
 
-    def set_image(self, image):
-        self.img = image
+    def set_image(self, img):
+        self.img = img
 
-    def extract_green(self):
-        img_lab = cv2.cvtColor(self.img, cv2.COLOR_BGR2LAB)
-        l_green = np.array([35, 60, 60], dtype=np.uint8)
-        h_green = np.array([80, 255, 255], dtype=np.uint8)
+    def get_image(self):
+        return self.img
 
-        self.green_layer = cv2.inRange(img_lab, l_green, h_green)
+    def get_plants_coordinates(self):
+        return self.plants_centers
 
-    def process_image(self, thresh_v):
-        img = cv2.GaussianBlur(self.green_layer, (11, 11), -1)
-        self.green_threshed = cv2.threshold(
-                img,
-                thresh_v,
+    def identify_plants(self, thresh_value, min_area):
+
+        green_layer = self.img[:, :, 1]
+        _, g_thresh = cv2.threshold(
+                green_layer,
+                thresh_value,
                 255,
                 cv2.THRESH_BINARY)
-
-    def extract_coordinates(self, min_a):
-        conts, _ = cv2.findContours(
-                self.green_threshed,
+        plants, _ = cv2.findContours(
+                g_thresh,
                 cv2.RETR_EXTERNAL,
                 cv2.CHAIN_APPROX_NONE)
+        plants = [x for x in plants if cv2.contourArea(x) > min_area]
 
-        conts = [cont for cont in conts if cv2.contourArea(cont) > min_a]
+        self.plants_centers = []
 
-        x_coord = []
-        y_coord = []
+        for plant in plants:
+            center, _ = cv2.minEnclosingCircle(plant)
+            self.plants_centers.append(tuple(map(int, center)))
 
-        for cont in conts:
-            x, y, w, h = cv2.boundingRect(cont)
-            y_coord.append((y + h) / 2)
-            x_coord((x + w) / 2)
-
-        return x_coord, y_coord
-
-    def find_plants(self, image, thresh_v, min_a):
-
-        self.set_image(image)
-        self.extract_green()
-        self.process_image(thresh_v)
-        x_coord, y_coord, = self.extract_coordinates(min_a)
-
-        return x_coord, y_coord
+        cv2.drawContours(
+                self.img,
+                plants,
+                -1,
+                (0, 255, 0),
+                2)
